67d60e167b1cd5ebdcd90bbbd6f5b5d0c97279bf
Luis Sanchez <luisfer1299sj@gmail.com>
2022-04-05 16:30:25 -0500

    Configuraciones basicas e instalacion de librerias

---
+++ src/redux/index.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+    user: 'none'
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+		switch(action.type){
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
